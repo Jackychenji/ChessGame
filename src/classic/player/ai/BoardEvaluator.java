27719d891f172e232eb22bc541d5c086b627ede2
nwhvlewbvuerwv <96509115+nwhvlewbvuerwv@users.noreply.github.com>
2022-05-17 19:07:21 +0800

    Michael's draft

---
+++ src/classic/player/ai/BoardEvaluator.java
@@ -0,0 +1,9 @@
+package classic.player.ai;
+
+import InternationalChess.engine.classic.board.Board;
+
+public interface BoardEvaluator {
+
+    int evaluate(Board board, int depth);
+
+}
---
+++ src/classic/player/ai/MoveStrategy.java
@@ -0,0 +1,12 @@
+package classic.player.ai;
+
+import InternationalChess.engine.classic.board.Board;
+import InternationalChess.engine.classic.board.Move;
+
+public interface MoveStrategy {
+
+    long getNumBoardsEvaluated();
+
+    Move execute(Board board);
+
+}
